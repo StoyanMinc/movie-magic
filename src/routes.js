2f93b93f328a14e036a17b0ad2aaf7d275e3de70
StoyanMinc <smminchev@abv.bg>
2024-12-01 11:41:10 +0200

    Add 404 page

--- src/routes.js
+++ src/routes.js
@@ -4,4 +4,7 @@ const homeController = require('./controllers/homeController')
 
 router.use(homeController);
 
+router.get('*', (req, res) => {
+    res.redirect('/404');
+});
 module.exports = router;
