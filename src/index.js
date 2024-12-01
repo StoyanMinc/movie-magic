9e5a189648b902cb69021c83dab78ba15da50a7f
StoyanMinc <smminchev@abv.bg>
2024-12-01 11:07:02 +0200

    Set controllers

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const exp = require('constants');
 const express = require('express');
 const handlebars = require('express-handlebars');
 const path = require('path');
+const router = require('./routes');
 
 const PORT = 5000
 
@@ -10,12 +11,11 @@ app.engine('hbs', handlebars.engine({
     extname: 'hbs'
 }));
 app.set('view engine', 'hbs');
+
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-    res.render('home', { layout: false });
-});
+app.use(router);
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
