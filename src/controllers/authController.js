b580749a88055c87140e50b078f91c0797859ccd
StoyanMinc <smminchev@abv.bg>
2024-12-09 09:02:41 +0200

    Add registered user to db

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,11 +1,21 @@
 const router = require('express').Router();
 
+const { register } = require('../service/authService');
+
 router.get('/login', (req, res) => {
     res.render('auth/login');
 });
 
 router.get('/register', (req, res) => {
     res.render('auth/register');
+});
+
+router.post('/register', async (req, res) => {
+   const userData = req.body;
+
+    await register({ userData });
+    
+    res.redirect('/auth/login');
 });
 
 module.exports = router;
--- src/models/User.js
+++ src/models/User.js
@@ -1,15 +1,18 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-    name: {
+    email: {
         type: String,
-        required: true
+        required: true,
+        lowercase: true
     },
     password: {
         type: String,
         required: true
     }
 });
+
+
 
 const User = mongoose.model('User', userSchema);
 
